b4bf7dd38964263e48772baab9032c7056dea4e7
Fossa <Fossa7@mail.ru>
2026-03-26 03:20:53 +1000

    ячсмит

---
+++ deployApp.R
@@ -0,0 +1,7 @@
+install.packages('rsconnect')
+rsconnect::setAccountInfo(name='fossa7',
+                          token='A82B9E9A6C8DA359B5381739A6372ABB',
+                          secret='cZg7DDY1bZPjeunp3ihpEVmkq0wMJX2mVt2sUtiP')
+
+library(rsconnect)
+rsconnect::deployApp('C:/Users/Fossa/OneDrive/Документы/WLshiny')
